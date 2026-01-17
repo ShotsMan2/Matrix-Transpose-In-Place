ca2fdae4cdec7e859a29475f99422b8c293b1972
Selim Efe <uysalselimefe@gmail.com>
2026-01-17 22:01:43 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,41 +4,49 @@
 
 int main()
 {
-       int i,j,a,n,yukarimi;
+    int i, j, a, n, yukarimi;
 
     printf("n i girin ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
     int matris[n][n];
 
-    a=1;
+    a = 1;
 
-    yukarimi=1;
+    yukarimi = 1;
 
-    for(i=n-1;i>=0;i--){
-        if(yukarimi==1){
-            for(j=n-1;j>=0;j--){
-                matris[j][i]=a++;
+    for (i = n - 1; i >= 0; i--)
+    {
+        if (yukarimi == 1)
+        {
+            for (j = n - 1; j >= 0; j--)
+            {
+                matris[j][i] = a++;
             }
         }
-        else{
-            for(j=0;j<n;j++){
-                matris[j][i]=a++;
+        else
+        {
+            for (j = 0; j < n; j++)
+            {
+                matris[j][i] = a++;
             }
         }
-        if(yukarimi==1){
-            yukarimi=0;
+        if (yukarimi == 1)
+        {
+            yukarimi = 0;
         }
-        else{
-            yukarimi=1;
+        else
+        {
+            yukarimi = 1;
         }
     }
 
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            printf("%3d",matris[i][j]);
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%3d", matris[i][j]);
         }
         printf("\n");
     }
-
 }
